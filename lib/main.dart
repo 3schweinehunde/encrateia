042d9d9fa5c702a2d6adcf2006725c870f9fe153
stefan Haslinger <mail@stefan-haslinger.at>
2019-08-15 15:39:05 +0200

    dashboard with help and create new athlete card widgets

--- lib/main.dart
+++ lib/main.dart
@@ -17,18 +17,64 @@ class MyApp extends StatelessWidget {
 
 class Dashboard extends StatelessWidget {
   final String title;
+  Widget widget;
 
   Dashboard({this.title});
 
   @override
   Widget build(BuildContext context) {
+    if (true) {
+      widget = ListView(padding: EdgeInsets.all(20), children: <Widget>[
+      Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+              leading: Icon(Icons.help),
+              title: Text('Welcome to Encrateia!'),
+              subtitle: Text('Maybe you want to learn more about Encrateia.'
+              'We have provided some introductory help for you.')
+          ),
+          ButtonTheme.bar(
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: const Text('HELP'),
+                    onPressed: () {},
+                  )
+                ],
+              ))
+        ],
+      )),
+
+        Card(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+                leading: Icon(Icons.face),
+                title: Text('Who are you?'),
+                subtitle: Text('This app stores date associated to one athlete '
+                    '(you) or many athletes (if you act as a trainer).'
+                    '\n Please create your first athlete!')),
+            ButtonTheme.bar(
+                child: ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: const Text('CREATE NEW ATHLETE'),
+                  onPressed: () {},
+                )
+              ],
+            ))
+          ],
+        ))
+      ]);
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text('Hi')
-      ),
-    );
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: widget);
   }
 }
