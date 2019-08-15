1e727dfe96367392a5624f9b0f3d07b2ab6db716
stefan Haslinger <mail@stefan-haslinger.at>
2019-08-15 15:01:31 +0200

    set application launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -8,54 +8,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Encrateia',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.orange, // #ff9800
       ),
-      home: MyHomePage(title: 'Encrateia Start'),
+      home: Dashboard(title: 'Encrateia Dashboard'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class Dashboard extends StatelessWidget {
   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Dashboard({this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Text('Hi')
       ),
     );
   }
