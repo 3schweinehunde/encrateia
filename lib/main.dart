f4964956eae410fd991cf9037b8ebcdc4571cbe3
stefan Haslinger <mail@stefan-haslinger.at>
2020-03-18 08:27:25 +0100

    started to develop local file pickup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/dashboard.dart';
 import 'package:encrateia/utils/db.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Db.create().connect();
+
+  var directories = await getExternalStorageDirectories();
+  var hintFile = File('${directories[0].path}/put_your_fit_files_here.txt');
+  await hintFile.writeAsString('This is the directory where Encrateia can pickup .fit-files from.');
   runApp(MyApp());
 }
 
