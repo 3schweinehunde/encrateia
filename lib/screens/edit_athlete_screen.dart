0418a0b788daaadb3c86453c0fc1c83356d2e043
Stefan Haslinger <stefan.haslinger@mittenin.at>
2020-07-08 15:25:26 +0200

    removed unused imports

--- lib/screens/edit_athlete_screen.dart
+++ lib/screens/edit_athlete_screen.dart
@@ -1,13 +1,8 @@
-import 'package:encrateia/models/strava_fit_download.dart';
-import 'package:encrateia/utils/my_button.dart';
 import 'package:encrateia/utils/my_color.dart';
 import 'package:encrateia/widgets/athlete_widgets/edit_standalone_athlete_widget.dart';
 import 'package:encrateia/widgets/athlete_widgets/edit_strava_athlete_widget.dart';
-import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:encrateia/models/athlete.dart';
-import 'package:encrateia/utils/icon_utils.dart';
-import 'strava_get_user.dart';
 
 class EditAthleteScreen extends StatelessWidget {
   const EditAthleteScreen({
--- lib/screens/onboarding_screens/onboarding_strava_credentials_screen.dart
+++ lib/screens/onboarding_screens/onboarding_strava_credentials_screen.dart
@@ -1,13 +1,7 @@
-import 'package:encrateia/models/strava_fit_download.dart';
-import 'package:encrateia/utils/my_button.dart';
 import 'package:encrateia/utils/my_color.dart';
 import 'package:encrateia/widgets/athlete_widgets/edit_strava_athlete_widget.dart';
-import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:encrateia/models/athlete.dart';
-import 'package:encrateia/utils/icon_utils.dart';
-
-import 'onboarding_power_zone_schema_screen.dart';
 
 class OnBoardingStravaCredentialsScreen extends StatelessWidget {
   const OnBoardingStravaCredentialsScreen({
