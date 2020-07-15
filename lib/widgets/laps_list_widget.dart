042e99e6e74262a421f450a4052ad9fa40eff320
stefan Haslinger <mail@stefan-haslinger.at>
2020-07-15 13:40:42 +0200

    lap list scrolling vertically

--- lib/widgets/laps_list_widget.dart
+++ lib/widgets/laps_list_widget.dart
@@ -23,50 +23,52 @@ class LapsListWidget extends StatelessWidget {
         if (snapshot.hasData) {
           final List<Lap> laps = snapshot.data;
           return SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: DataTable(
-              showCheckboxColumn: false,
-              onSelectAll: (_) {},
-              columns: const <DataColumn>[
-                DataColumn(label: Text('Lap'), numeric: true),
-                DataColumn(label: Text('HR\nbpm'), numeric: true),
-                DataColumn(label: Text('Pace\nmin:s'), numeric: true),
-                DataColumn(label: Text('Power\nWatt'), numeric: true),
-                DataColumn(label: Text('Dist.\nkm'), numeric: true),
-                DataColumn(label: Text('Ascent\nm'), numeric: true),
-              ],
-              rows: laps.map((Lap lap) {
-                return DataRow(
-                  key: ValueKey<int>(lap.id),
-                  onSelectChanged: (bool selected) {
-                    if (selected) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute<BuildContext>(
-                          builder: (BuildContext context) => ShowLapScreen(
-                            lap: lap,
-                            laps: laps,
-                            athlete: athlete,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                showCheckboxColumn: false,
+                onSelectAll: (_) {},
+                columns: const <DataColumn>[
+                  DataColumn(label: Text('Lap'), numeric: true),
+                  DataColumn(label: Text('HR\nbpm'), numeric: true),
+                  DataColumn(label: Text('Pace\nmin:s'), numeric: true),
+                  DataColumn(label: Text('Power\nWatt'), numeric: true),
+                  DataColumn(label: Text('Dist.\nkm'), numeric: true),
+                  DataColumn(label: Text('Ascent\nm'), numeric: true),
+                ],
+                rows: laps.map((Lap lap) {
+                  return DataRow(
+                    key: ValueKey<int>(lap.id),
+                    onSelectChanged: (bool selected) {
+                      if (selected) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute<BuildContext>(
+                            builder: (BuildContext context) => ShowLapScreen(
+                              lap: lap,
+                              laps: laps,
+                              athlete: athlete,
+                            ),
                           ),
-                        ),
-                      );
-                    }
-                  },
-                  cells: <DataCell>[
-                    DataCell(Text(lap.index.toString())),
-                    DataCell(Text(avgHeartRateString(lap.avgHeartRate))),
-                    DataCell(Text(lap.avgSpeed.toPace())),
-                    DataCell(Text(lap.avgPower.toStringOrDashes(1))),
-                    DataCell(
-                      Text((lap.totalDistance / 1000).toStringAsFixed(2)),
-                    ),
-                    DataCell(
-                      Text((lap.totalAscent - lap.totalDescent)
-                          .toString()),
-                    ),
-                  ],
-                );
-              }).toList(),
+                        );
+                      }
+                    },
+                    cells: <DataCell>[
+                      DataCell(Text(lap.index.toString())),
+                      DataCell(Text(avgHeartRateString(lap.avgHeartRate))),
+                      DataCell(Text(lap.avgSpeed.toPace())),
+                      DataCell(Text(lap.avgPower.toStringOrDashes(1))),
+                      DataCell(
+                        Text((lap.totalDistance / 1000).toStringAsFixed(2)),
+                      ),
+                      DataCell(
+                        Text((lap.totalAscent - lap.totalDescent)
+                            .toString()),
+                      ),
+                    ],
+                  );
+                }).toList(),
+              ),
             ),
           );
         } else {
