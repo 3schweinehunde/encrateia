90794017f45f2d9e5ae50047abee4bc93935369b
Stefan Haslinger <stefan.haslinger@mittenin.at>
2020-09-23 16:52:27 +0200

    show record details on selection for lap pace screen

--- lib/widgets/charts/lap_charts/lap_pace_chart.dart
+++ lib/widgets/charts/lap_charts/lap_pace_chart.dart
@@ -1,10 +1,14 @@
 import 'package:charts_flutter/flutter.dart';
 import 'package:encrateia/models/record_list.dart';
+import 'package:encrateia/screens/show_event_screen.dart';
+import 'package:encrateia/utils/PQText.dart';
+import 'package:encrateia/utils/enums.dart';
 import 'package:encrateia/utils/graph_utils.dart';
+import 'package:encrateia/utils/my_button.dart';
 import 'package:flutter/material.dart';
 import 'package:encrateia/models/event.dart';
 
-class LapPaceChart extends StatelessWidget {
+class LapPaceChart extends StatefulWidget {
   const LapPaceChart({
     @required this.records,
     @required this.minimum,
@@ -16,8 +20,24 @@ class LapPaceChart extends StatelessWidget {
   final double maximum;
 
   @override
+  _LapPaceChartState createState() => _LapPaceChartState();
+}
+
+class _LapPaceChartState extends State<LapPaceChart> {
+  Event selectedEvent;
+
+  void _onSelectionChanged(SelectionModel<num> model) {
+    final List<SeriesDatum<dynamic>> selectedDatum = model.selectedDatum;
+
+    if (selectedDatum.isNotEmpty) {
+      setState(() => selectedEvent = selectedDatum[0].datum as Event);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final int offset = records.first.distance.round();
+    final int offset = widget.records.first.distance.round();
+    final DateTime startingTime = widget.records.first.timeStamp;
 
     final List<Series<Event, int>> data = <Series<Event, int>>[
       Series<Event, int>(
@@ -25,33 +45,81 @@ class LapPaceChart extends StatelessWidget {
         colorFn: (_, __) => MaterialPalette.black,
         domainFn: (Event record, _) => record.distance.round() - offset,
         measureFn: (Event record, _) => 50 / 3 / record.speed,
-        data: records,
+        data: widget.records,
       )
     ];
 
-    return AspectRatio(
-      aspectRatio:
-          MediaQuery.of(context).orientation == Orientation.portrait ? 1 : 2,
-      child: LineChart(
-        data,
-        defaultRenderer: LineRendererConfig<num>(
-          includeArea: true,
-          strokeWidthPx: 1,
-        ),
-        primaryMeasureAxis: NumericAxisSpec(
-          tickProviderSpec: const BasicNumericTickProviderSpec(
-            zeroBound: false,
-            dataIsInWholeNumbers: false,
-            desiredTickCount: 5,
+    return Column(children: <Widget>[
+      AspectRatio(
+        aspectRatio:
+            MediaQuery.of(context).orientation == Orientation.portrait ? 1 : 2,
+        child: LineChart(
+          data,
+          defaultRenderer: LineRendererConfig<num>(
+            includeArea: true,
+            strokeWidthPx: 1,
           ),
-          viewport: NumericExtents(minimum, maximum),
+          primaryMeasureAxis: NumericAxisSpec(
+            tickProviderSpec: const BasicNumericTickProviderSpec(
+              zeroBound: false,
+              dataIsInWholeNumbers: false,
+              desiredTickCount: 5,
+            ),
+            viewport: NumericExtents(widget.minimum, widget.maximum),
+          ),
+          animate: false,
+          behaviors: GraphUtils.axis(
+            measureTitle: 'Pace (min/km)',
+          ),
+          flipVerticalAxis: true,
+          selectionModels: <SelectionModelConfig<num>>[
+            SelectionModelConfig<num>(
+              type: SelectionModelType.info,
+              changedListener: _onSelectionChanged,
+            )
+          ],
         ),
-        animate: false,
-        behaviors: GraphUtils.axis(
-          measureTitle: 'Pace (min/km)',
-        ),
-        flipVerticalAxis: true,
       ),
-    );
+      if (selectedEvent != null)
+        Column(
+          children: <Widget>[
+            const Divider(),
+            const Text('Selected Record:'),
+            ListTile(
+              title: PQText(
+                value: selectedEvent.timeStamp,
+                pq: PQ.dateTime,
+                format: DateTimeFormat.longDateTime,
+              ),
+              subtitle: const Text('Point in time'),
+            ),
+            ListTile(
+              title: PQText(
+                value:
+                    selectedEvent.timeStamp.difference(startingTime).inSeconds,
+                pq: PQ.duration,
+              ),
+              subtitle: const Text('Time elapsed'),
+            ),
+            ListTile(
+              title: PQText(
+                  value: selectedEvent.distance, pq: PQ.distanceInMeters),
+              subtitle: const Text('Distance'),
+            ),
+            MyButton.detail(
+              child: const Text('Details'),
+              onPressed: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute<BuildContext>(
+                      builder: (BuildContext context) =>
+                          ShowEventScreen(record: selectedEvent)),
+                );
+              },
+            ),
+            const Divider(),
+          ],
+        )
+    ]);
   }
 }
