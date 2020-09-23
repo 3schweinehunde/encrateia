732f8387c72ae0d27ed1f47276b34221d1ae4b5f
Stefan Haslinger <stefan.haslinger@mittenin.at>
2020-09-23 17:06:44 +0200

    optimize record viewer for smaller devices

--- lib/widgets/activity_widgets/activity_record_list_widget.dart
+++ lib/widgets/activity_widgets/activity_record_list_widget.dart
@@ -40,138 +40,151 @@ class _ActivityRecordListWidgetState extends State<ActivityRecordListWidget> {
     if (records.isNotEmpty) {
       rows = (records.length < 8) ? records.length : 8;
       final DateTime startingTime = records.first.timeStamp;
-      return Column(
-        children: <Widget>[
-          DataTable(
-            columns: const <DataColumn>[
-              DataColumn(label: Text('Point in time')),
-              DataColumn(label: Text('Time elapsed')),
-              DataColumn(
-                label: Text('Distance'),
-                numeric: true,
-              ),
-              DataColumn(label: Text('Details')),
-            ],
-            rows: records.sublist(offset, offset + rows).map((Event record) {
-              return DataRow(
-                key: ValueKey<int>(record.id),
-                cells: <DataCell>[
-                  DataCell(PQText(
-                    value: record.timeStamp,
-                    pq: PQ.dateTime,
-                    format: DateTimeFormat.longDateTime,
-                  )),
-                  DataCell(PQText(
-                    value: record.timeStamp.difference(startingTime).inSeconds,
-                    pq: PQ.duration,
-                  )),
-                  DataCell(
-                      PQText(value: record.distance, pq: PQ.distanceInMeters)),
-                  DataCell(
-                    MyIcon.show,
-                    onTap: () async {
-                      await Navigator.push(
-                        context,
-                        MaterialPageRoute<BuildContext>(
-                            builder: (BuildContext context) =>
-                                ShowEventScreen(record: record)),
-                      );
-                      getData();
-                    },
-                  )
-                ],
-              );
-            }).toList(),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              '${offset + 1} - ${offset + rows} '
-              'of ${records.length} (Page ${(offset / 8).round() + 1} of ${(records.length / 8).round()})',
-              textAlign: TextAlign.right,
-            ),
-          ),
-          Wrap(
+      return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: SingleChildScrollView(
+          child: Column(
             children: <Widget>[
-              const SizedBox(width: 20),
-              MyButton.navigate(
-                  child: const Text('|<'),
-                  onPressed:
-                      (offset == 0) ? null : () => setState(() => offset = 0)),
-              const SizedBox(width: 20),
-              MyButton.navigate(
-                child: const Text('100 <'),
-                onPressed: (offset == 0)
-                    ? null
-                    : () => setState(() {
-                          offset > 800
-                              ? offset = offset - 100 * rows
-                              : offset = 0;
-                        }),
+              DataTable(
+          columnSpacing: 10,
+                columns: const <DataColumn>[
+                  DataColumn(label: Text('Point in time')),
+                  DataColumn(label: Text('Time elapsed')),
+                  DataColumn(
+                    label: Text('Distance'),
+                    numeric: true,
+                  ),
+                  DataColumn(label: Text('Details')),
+                ],
+                rows:
+                    records.sublist(offset, offset + rows).map((Event record) {
+                  return DataRow(
+                    key: ValueKey<int>(record.id),
+                    cells: <DataCell>[
+                      DataCell(PQText(
+                        value: record.timeStamp,
+                        pq: PQ.dateTime,
+                        format: DateTimeFormat.longDateTime,
+                      )),
+                      DataCell(PQText(
+                        value:
+                            record.timeStamp.difference(startingTime).inSeconds,
+                        pq: PQ.duration,
+                      )),
+                      DataCell(PQText(
+                          value: record.distance, pq: PQ.distanceInMeters)),
+                      DataCell(
+                        MyIcon.show,
+                        onTap: () async {
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute<BuildContext>(
+                                builder: (BuildContext context) =>
+                                    ShowEventScreen(record: record)),
+                          );
+                          getData();
+                        },
+                      )
+                    ],
+                  );
+                }).toList(),
               ),
-              const SizedBox(width: 20),
-              MyButton.navigate(
-                child: const Text('10 <'),
-                onPressed: (offset == 0)
-                    ? null
-                    : () => setState(() {
-                          offset > 80
-                              ? offset = offset - 10 * rows
-                              : offset = 0;
-                        }),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  '${offset + 1} - ${offset + rows} '
+                  'of ${records.length} (Page ${(offset / 8).round() + 1} of ${(records.length / 8).round()})',
+                  textAlign: TextAlign.right,
+                ),
               ),
-              const SizedBox(width: 20),
-              MyButton.navigate(
-                child: const Text('<'),
-                onPressed: (offset == 0)
-                    ? null
-                    : () => setState(() {
-                          offset > 8 ? offset = offset - rows : offset = 0;
-                        }),
+              Row(
+                children: <Widget>[
+                  const SizedBox(width: 10),
+                  MyButton.navigate(
+                      child: const Text('|<'),
+                      onPressed: (offset == 0)
+                          ? null
+                          : () => setState(() => offset = 0)),
+                  const SizedBox(width: 10),
+                  MyButton.navigate(
+                    child: const Text('<'),
+                    onPressed: (offset == 0)
+                        ? null
+                        : () => setState(() {
+                              offset > 8 ? offset = offset - rows : offset = 0;
+                            }),
+                  ),
+                  const SizedBox(width: 10),
+                  MyButton.navigate(
+                    child: const Text('>'),
+                    onPressed: (offset + rows == records.length)
+                        ? null
+                        : () => setState(() {
+                              offset + rows < records.length - rows
+                                  ? offset = offset + rows
+                                  : offset = records.length - rows;
+                            }),
+                  ),
+                  const SizedBox(width: 10),
+                  MyButton.navigate(
+                    child: const Text('|>'),
+                    onPressed: (offset + rows == records.length)
+                        ? null
+                        : () => setState(() => offset = records.length - rows),
+                  ),
+                ],
               ),
-              const SizedBox(width: 20),
-              MyButton.navigate(
-                child: const Text('>'),
-                onPressed: (offset + rows == records.length)
-                    ? null
-                    : () => setState(() {
-                          offset + rows < records.length - rows
-                              ? offset = offset + rows
-                              : offset = records.length - rows;
-                        }),
-              ),
-              const SizedBox(width: 20),
-              MyButton.navigate(
-                child: const Text('10 >'),
-                onPressed: (offset + rows == records.length)
-                    ? null
-                    : () => setState(() {
-                          offset + 10 * rows < records.length - rows
-                              ? offset = offset + 10 * rows
-                              : offset = records.length - rows;
-                        }),
-              ),
-              const SizedBox(width: 20),
-              MyButton.navigate(
-                child: const Text('100 >'),
-                onPressed: (offset + rows == records.length)
-                    ? null
-                    : () => setState(() {
-                          offset + 100 * rows < records.length - rows
-                              ? offset = offset + 100 * rows
-                              : offset = records.length - rows;
-                        }),
-              ),
-              const SizedBox(width: 20),
-              MyButton.navigate(
-                child: const Text('|>'),
-                onPressed: (offset + rows == records.length)
-                    ? null
-                    : () => setState(() => offset = records.length - rows),
+              Row(
+                children: <Widget>[
+                  const SizedBox(width: 10),
+                  MyButton.navigate(
+                    child: const Text('100 <'),
+                    onPressed: (offset == 0)
+                        ? null
+                        : () => setState(() {
+                              offset > 800
+                                  ? offset = offset - 100 * rows
+                                  : offset = 0;
+                            }),
+                  ),
+                  const SizedBox(width: 10),
+                  MyButton.navigate(
+                    child: const Text('10 <'),
+                    onPressed: (offset == 0)
+                        ? null
+                        : () => setState(() {
+                              offset > 80
+                                  ? offset = offset - 10 * rows
+                                  : offset = 0;
+                            }),
+                  ),
+                  const SizedBox(width: 10),
+                  MyButton.navigate(
+                    child: const Text('10 >'),
+                    onPressed: (offset + rows == records.length)
+                        ? null
+                        : () => setState(() {
+                              offset + 10 * rows < records.length - rows
+                                  ? offset = offset + 10 * rows
+                                  : offset = records.length - rows;
+                            }),
+                  ),
+                  const SizedBox(width: 10),
+                  MyButton.navigate(
+                    child: const Text('100 >'),
+                    onPressed: (offset + rows == records.length)
+                        ? null
+                        : () => setState(() {
+                              offset + 100 * rows < records.length - rows
+                                  ? offset = offset + 100 * rows
+                                  : offset = records.length - rows;
+                            }),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       );
     } else {
       return Center(
