f59248d59a1278b2914b1b25648e7f712109bf06
Stefan Haslinger <stefan.haslinger@mittenin.at>
2022-02-11 11:10:44 +0100

    switched to flutter from snap, back to android studio

--- lib/screens/add_filter_screen.dart
+++ lib/screens/add_filter_screen.dart
@@ -4,7 +4,6 @@ import 'package:encrateia/models/tag_group.dart';
 import 'package:encrateia/utils/my_button.dart';
 import 'package:encrateia/utils/my_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class AddFilterScreen extends StatefulWidget {
